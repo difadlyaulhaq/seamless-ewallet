e5e3e2d1d4e424844aeb05ec9ea388747048c38e
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2025-04-15 09:32:04 +0700

    Enhance onboarding page layout and content; improve carousel item display and add descriptive text

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -17,11 +17,53 @@ class OnboardingPage extends StatelessWidget {
               items: [
                 Image.asset(
                   "assets/image_onboarding_1.png",
-                )
+                  height: 300,
+                ),
+                Image.asset(
+                  "assets/image_onboarding_2.png",
+                  height: 300,
+                ),
+                Image.asset(
+                  "assets/image_onboarding_3.png",
+                  height: 300,
+                ),
               ], 
               options: CarouselOptions(
-
+                height: 300,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: false,
+              
               )
+            ),
+            const SizedBox(height: 80,),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              padding: EdgeInsets.symmetric(vertical: 24, horizontal: 22),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 Text(
+                  "Grow your financial Today", 
+                 style: blackTextStyle.copyWith(
+                 fontSize: 20, 
+                 fontWeight: semiBold
+                 ),
+                ),
+                const SizedBox(height: 26),
+                Text(
+                  "We provide a full service to you to help you grow your business and increase your income", 
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16, 
+                    fontWeight: regular,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+              ),
             )
           ],
         ),
